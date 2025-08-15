36869ff1119140f9884e65a8a2a8f8d26a2f888b
sonu11474 <sonuyadav11474@gmail.com>
2025-08-16 00:56:48 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,289 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// // Experience Section Logo's
+// import webverseLogo from './assets/company_logo/webverse_logo.png';
+// import agcLogo from './assets/company_logo/agc_logo.png';
+// import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import cuLogo from './assets/education_logo/culogo.png';
+import addarshLogo from './assets/education_logo/addarsh.png';
+import munnamLogo from './assets/education_logo/mps_logo.png';
+
+// Project Section Logo's
+import employeeLogo from './assets/work_logo/Employee_management.png';
+//import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import AnalogclockLogo from './assets/work_logo/Analog_clock.png';
+import AmazonLogo from './assets/work_logo/Amazon.png';
+import backgroundLogo from './assets/work_logo/background.png';
+//import cmLogo from './assets/work_logo/cm.png';
+//import imagesearchLogo from './assets/work_logo/image_search.png';
+//import removebgLogo from './assets/work_logo/remove_bg.png';
+import WeatherLogo from './assets/work_logo/Weather.png';
+//import { PiAmazonLogo } from 'react-icons/pi';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'Java', logo: javaLogo },
+      { name: 'SQL', logo: mysqlLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+    ],
+  },
+];
+
+  // export const experiences = [
+  //   {
+  //     id: 0,
+  //     img: webverseLogo,
+  //     role: "Fullstack Developer",
+  //     company: "Webverse Digital",
+  //     date: "April 2024 - Present",
+  //     desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //       "React JS",
+  //       "TypeScript",
+  //       "Node JS",
+  //       "Tailwind CSS",
+  //       "MongoDb",
+  //       "Redux",
+  //       " Next Js",
+  //     ],
+  //   },
+  //   {
+  //     id: 1,
+  //     img: agcLogo,
+  //     role: "Fullstack Engineer",
+  //     company: "Agumentik Group of Companies",
+  //     date: "July 2023 - March 2024",
+  //     desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+  //     skills: [
+  //       "ReactJS",
+  //       "Redux",
+  //       "JavaScript",
+  //       "Tailwind CSS",
+  //       "HTML",
+  //       "CSS",
+  //       "SQL",
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     img: newtonschoolLogo,
+  //     role: "Frontend Intern",
+  //     company: "Newton School",
+  //     date: "September 2021 - August 2022",
+  //     desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "Javascript",
+  //       "Bootstrap",
+  //       "Figma",
+  //       "Material UI",
+  //     ],
+  //   },
+  // ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: cuLogo,
+      school: "Chandigarh University,Punjab ",
+      date: "Aug 2022 - July 2026",
+      grade: "8.05 CGPA",
+      desc: "I have completed my Bachelor of Engineering (B.E.) in Computer Science and Engineering from Chandigarh University. During my time at CU, I gained a strong foundation in programming, software development, and core computer science principles. I studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops, hackathons, and technical events, which enhanced my skills and practical knowledge. My experience at Chandigarh University has been instrumental in shaping my technical abilities and preparing me for a successful career in the field of technology.",
+      degree: "Bachelor of Engineering (B.E.) in Computer Science and Engineering - CSE",
+    },
+    {
+      id: 1,
+      img: addarshLogo,
+      school: "ADARSH +2 UCHCHA VIDYALAYA, Madhwatand,Jharkhand",
+      date: "Apr 2020 - March 2021",
+      grade: "71%",
+      desc: "I completed my Class 12 education from ADARSH +2 Uchcha Vidyalaya, Madhwatand, Jharkhand, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM).",
+      degree: "CBSE(XII) - Physic Chemistry Math(PCM)",
+    },
+    {
+      id: 2,
+      img: munnamLogo,
+      school: "Munam Public School,Hazaribagh,Jharkhand",
+      date: "Apr 2018 - March 2019",
+      grade: "82.5%",
+      desc: " I completed my Class 10 education from Munam Public School, under the CBSE board, where I studied Science, Mathematics, and other core subjects.",
+      degree: "CBSE(X)",
+    },
+    // {
+    //   id: 3,
+    //   img: vpsLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2015 - March 2016",
+    //   grade: "87.5%",
+    //   desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    //   degree: "CBSE(X), Science with Computer Application",
+    // },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Employee Management System (AWT/Swing)",
+      description:
+        "Developed a desktop-based application using Java AWT and Swing to manage employee records with features likeadd, update, delete, and search.",
+      image: employeeLogo,
+      tags: ["AWT","SWING","IntelliJ IDEA","MySQL","SQL","Java."],
+      github: "https://github.com/sonu11474/Employee-management-system",
+      // webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Weather App",
+      description:
+        "Built a weather app using HTML, CSS, and JavaScript, integrated with the OpenWeather API to display real-time weather details like temperature, humidity, and conditions.",
+      image: WeatherLogo,
+      tags: ["API integration" ,"HTML", "CSS", "JavaScript"],
+      github: "https://github.com/sonu11474/Weather-App",
+      webapp: "https://sonu11474.github.io/Weather-App/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Amazone-Clone",
+      description:
+        "Built an Amazon clone with HTML, CSS, and JavaScript featuring responsive design, product listings, and interactive UI elements.",
+      image:AmazonLogo,
+      tags: ["HTML", "CSS","JavaScript"],
+      github: "https://github.com/sonu11474/Amazone-Clone",
+      webapp: "https://sonu11474.github.io/Amazone-Clone/",
+    },
+    {
+      id: 4,
+      title: "Analog-clock",
+      description:
+        "Built an analog clock using HTML, CSS, and JavaScript, integrated with an API to display real-time accurate time.",
+      image: AnalogclockLogo,
+      tags: ["JavaScript", "HTML", "CSS"],
+      github: "https://github.com/sonu11474/Analog-clock",
+      webapp: "https://sonu11474.github.io/Analog-clock/",
+    },
+    {
+      id: 5,
+      title: "Background colour changer",
+      description:
+        "Built a background color changer using HTML, CSS, and JavaScript, allowing dynamic color changes with user interaction.",
+      image: backgroundLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/sonu11474/Background_colour_changer",
+      webapp: "https://sonu11474.github.io/Background_colour_changer/",
+    },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
