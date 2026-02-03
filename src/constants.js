6dd010f89a8d794eb580601285e86395d8429e07
sonu kumar yadav <149679385+sonu11474@users.noreply.github.com>
2026-02-03 17:29:10 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -54,7 +54,7 @@ import backgroundLogo from './assets/work_logo/background.png';
 //import imagesearchLogo from './assets/work_logo/image_search.png';
 //import removebgLogo from './assets/work_logo/remove_bg.png';
 import WeatherLogo from './assets/work_logo/weather.png';
-import WeatherLogo from './assets/work_logo/blogLogo.png';
+import blogLogo from './assets/work_logo/blogLogo.png';
 
 //import { PiAmazonLogo } from 'react-icons/pi';
 
